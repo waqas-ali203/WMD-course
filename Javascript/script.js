0077a5d35a510dc9752a5a55bab120104493c886
Waqas Ali <waqasansari4547@gmail.com>
2024-11-16 01:21:19 +0500

    WMD COURSE

---
+++ Javascript/script.js
@@ -0,0 +1,54 @@
+// add two numbers
+a=1;
+b=2;
+x=a+b;
+console.log(x);
+
+// subtract two numbers
+a=1;
+b=2;
+x=a-b;
+console.log(x);
+
+// multiply two numbers
+a=9;
+b=2;
+x=a*b;
+console.log(x);
+
+// divide two numbers
+a=10;
+b=2;
+x=a/b;
+console.log(x);
+
+// number is even or odd
+
+b=20;
+if (b%2 == 0){
+    console.log("B number is even")
+}
+else
+{
+console.log("B number is odd")
+}
+
+// Number is  divisible or not
+
+firstNumber=12;
+secondNumber=31;
+
+if(a%b == 0){
+    console.log("firstNumber number is divisible by secondNumber")
+}
+else 
+{
+    console.log("firstNumber number is NOT divisible by secondNumber")
+}
+
+// Check percentage of marks
+
+obtainMarks = 996;
+totalMarks = 1100;
+percentage = (obtainMarks/totalMarks)*100;
+console.log("The percentage of the student is " + percentage)
