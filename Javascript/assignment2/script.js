17ffc2238956a803c2eaab7b4a25c1f5029018ba
Waqas Ali <waqasansari4547@gmail.com>
2024-11-24 23:09:05 +0500

    updating file

---
+++ Javascript/assignment2/script.js
@@ -0,0 +1,262 @@
+// find maximun number between 2 numbers
+
+// var number1 =prompt("enter number1");
+// var number2 = prompt("enter number2");
+
+// if (number1 > number2){
+//     console.log("Number1 is greater than number2");
+// }
+// else
+// {
+//     console.log("Number2 is greater than number1");
+// }
+
+// find maximun number between 3 numbers
+
+// var number1 = prompt("Enter  number1");
+// var number2 = prompt("Enter  number2");
+// var number3 = prompt("Enter  number3");
+
+// if ( number1 > number2 & number1 > number3) {
+//     console.log("Number1 is greater");
+// }
+// else if (number2 > number3 & number2 > number3) {
+//     console.log("Number2 is greater");
+// }
+// else{
+//     console.log("Number3 is greater");
+// }
+
+// Numebr is positive or negative or zero
+
+// var number=prompt("Enter a number");
+
+// if (number > 0) {
+//     console.log("Your number is  positive");
+// }
+// else if (number < 0) {
+//     console.log("Your number is negative");
+// }
+// else {
+//     console.log("Your number is zero")
+// }
+
+// Numenr is divisble by 5 & 11 or not
+
+// var number = prompt("Enter you number");
+
+// if (number%5 == 0 || number%11 == 0) {
+//     console.log("Your number is divisible by given numbers");
+// }
+// else {
+//     console.log("Your number is not divisible by given numbers")
+// }
+
+// Number is even or odd
+
+// var number =prompt("Enter a number");
+
+// if (number%2 == 0) {
+//     console.log("Your number is even");
+// }
+// else {
+//     console.log("Your number is odd");
+// }
+
+// Number is a leap  year not
+
+// var year =prompt("Enter your year");
+
+// if (year%4 == 0) {
+//     console.log("Your year is a leap year");
+// }
+// else {
+//     console.log("Your year is not a leap year")
+// }
+
+// Character is a alphabet or not
+
+// var character=prompt("Enter a character");
+
+// if (character === "A" || character === "a" || character === "B" || character === "b" || character === "C" || character === "c" || character === "D" || character === "d" ||character === "E" || character === "e" ||character === "F" || character === "f" || character === "G" || character === "g" || character === "h" || character === "H" ||character === "i" || character === "I" ||character === "j" || character === "J" ||character === "K" || character === "k" ||character === "L" || character === "l" ||character === "M" || character === "m" ||character === "N" || character === "n" ||character === "O" || character === "o" ||character === "P" || character === "p" ||character === "q" || character === "Q" ||character === "R" || character === "r" ||character === "s" || character === "S" ||character === "T" || character === "t" ||character === "u" || character === "U" ||character === "V" || character === "v" ||character === "w" || character === "W" || character === "X" || character === "x" ||character === "y" || character === "Y" || character === "z" || character === "Z")
+//     {
+//     console.log("character is a alphabet");
+// }
+// else {
+//     console.log("character is not a alphabet");
+// }
+
+// Check input is vowel or not
+
+// var character = prompt("Enter character");
+
+// if (character === "A" || character === "a" || character === "E" || character ==="e" || character === "i" || character === "I" || character === "o" || character === "O" || character ==="u" || character === "U")
+// {
+//     console.log("Your character is a vowel");
+// }
+// else{
+//     console.log("Your character is not a vowel");
+// }
+
+// Alphabet is in upper case or lowerc case
+
+// var character = prompt("Enter a character");
+
+// if (
+//   character === "A" ||
+//   character === "B" ||
+//   character === "C" ||
+//   character === "D" ||
+//   character === "E" ||
+//   character === "F" ||
+//   character === "G" ||
+//   character === "H" ||
+//   character === "I" ||
+//   character === "J" ||
+//   character === "K" ||
+//   character === "L" ||
+//   character === "M" ||
+//   character === "N" ||
+//   character === "O" ||
+//   character === "P" ||
+//   character === "Q" ||
+//   character === "R" ||
+//   character === "S" ||
+//   character === "T" ||
+//   character === "U" ||
+//   character === "V" ||
+//   character === "W" ||
+//   character === "X" ||
+//   character === "Y" ||
+//   character === "Z"
+// ) {
+//   console.log("character is in upper case");
+// } else if (
+//   character === "a" ||
+//   character === "b" ||
+//   character === "c" ||
+//   character === "d" ||
+//   character === "e" ||
+//   character === "f" ||
+//   character === "g" ||
+//   character === "h" ||
+//   character === "i" ||
+//   character === "j" ||
+//   character === "k" ||
+//   character === "l" ||
+//   character === "m" ||
+//   character === "n" ||
+//   character === "o" ||
+//   character === "p" ||
+//   character === "q" ||
+//   character === "r" ||
+//   character === "s" ||
+//   character === "t" ||
+//   character === "u" ||
+//   character === "v" ||
+//   character === "w" ||
+//   character === "x" ||
+//   character === "y" ||
+//   character === "z"
+// ) {
+//   console.log("character is in lowercase ");
+// }
+// else {
+//     console.log("Your character is not a alphabet")
+// }
+
+// Numbe of that month
+
+// var month = prompt("Enter a month number");
+// var numberMonth = parseInt(month);
+
+// if (
+//   numberMonth == "1" ||
+//   numberMonth == "3" ||
+//   numberMonth == "5" ||
+//   numberMonth == "7" ||
+//   numberMonth == "8" ||
+//   numberMonth == "10" ||
+//   numberMonth == "12"
+// ) {
+//   console.log("Your month days is 31");
+// }
+// else {
+
+//     console.log("Your month days is 30");
+//  }
+
+// Total number of notes in amount
+
+// var totalAmount =prompt("Enter a total amount");
+// var noteType =prompt("Enter type of notes ");
+
+// var totalNotes =parseInt(totalAmount/noteType);
+
+// console.log("You have "+ totalNotes + " notes of  this " + noteType );
+
+// Angles are triangle or not
+
+// var firstAngle = parseInt(prompt("Enter a angle of triangle"));
+// var secondAngle =parseInt(prompt("Enter a angle of triangle"));
+// var thirdAngle =parseInt(prompt("Enter a angle of triangle"));
+// var sumOfAngles = firstAngle+secondAngle+thirdAngle;
+
+// if ( sumOfAngles == "180")
+// {
+//     console.log("You have valid triangle");
+// }
+// else{
+//     console.log("You have invalid triangle");
+// }
+
+// cechk Triangle type
+
+// var firstAngle = parseInt(prompt("Enter a angle of triangle"));
+// var secondAngle = parseInt(prompt("Enter a angle of triangle"));
+// var thirdAngle = parseInt(prompt("Enter a angle of triangle"));
+// var sumOfAngles = firstAngle+secondAngle+thirdAngle;
+// if (sumOfAngles== 180 ){
+
+// if (firstAngle == secondAngle && secondAngle == thirdAngle && thirdAngle == firstAngle) {
+//     console.log("You have Equieliant triangles");
+// }
+// else if (firstAngle == secondAngle || secondAngle == thirdAngle || thirdAngle == firstAngle)
+// {
+//     console.log("You have Isosceles triangles");
+// }
+// else {
+//     console.log("You have Scalene  triangles");
+// }
+// }
+// else {
+//     console.log("You have invalid triangle")
+// }
+
+// check the grade and percentage of subjects 
+
+// var physics = 85;
+// var chemistry = 90;
+// var biology = 78;
+// var mathematics = 88;
+// var computer = 92;
+// var grade;
+
+// var totalMarks = physics + chemistry + biology + mathematics + computer;
+// var percentage = (totalMarks / 500) * 100;
+
+// if (percentage >= 90) {
+//     grade = "A";
+// } else if (percentage >= 80) {
+//     grade = "B";
+// } else if (percentage >= 70) {
+//     grade = "C";
+// } else if (percentage >= 60) {
+//     grade = "D";
+// } else if (percentage >= 40) {
+//     grade = "E";
+// } else {
+//     grade = "F";
+// }
+// console.log("Percentage: " + percentage + "%");
+// console.log("Grade: " + grade);
---
+++ Javascript/assignment3/script.js
@@ -0,0 +1,98 @@
+// print all natural numbers 1-10
+
+// var number = 10;
+// var i = 1;
+
+// while (i <= number) {
+//     console.log(i);
+//     i++;
+// }
+
+
+// print all natural numbers in reverse 10-1
+
+// var number = 0;
+// var i = 10;
+
+// while (i > number) {
+//     console.log(i);
+//     i--;
+// }
+
+// print all alphabet from a to z
+// var letter = 'a';
+
+// while (letter <= 'z') {
+//     console.log(letter);
+    
+// }
+
+// print all even numbers from 1 to 100
+
+// var number = 2;
+
+// while (number <= 100) {
+//     console.log(number);
+//     number += 2;
+// }
+
+// print all odd numbers from 1 to 100
+
+// var number = 1;
+
+// while (number <= 100) {
+//     console.log(number);
+//     number += 2;
+// }
+
+
+// // print sum of all natural numbers from 1 to n
+
+// var n= 100;
+// var sum =0;
+// var i =1;
+
+// while (i <= n) {
+//     console.log(i);
+//     sum += i;
+//     i++;
+// }
+//  console.log("sum of all natural numbers " + sum);
+
+
+// // print sum of all even numbers from 1 to n
+
+// var n= 100;
+// var sum =0;
+// var i =2;
+
+// while (i <= n) {
+//     console.log(i);
+//     sum += i;
+//     i +=2;
+// }
+//  console.log("sum of all natural numbers " + sum);
+
+
+// // print sum of all odd numbers from 1 to n
+
+// var n= 100;
+// var sum =0;
+// var i =1;
+
+// while (i <= n) {
+//     console.log(i);
+//     sum += i;
+//     i +=2;
+// }
+//  console.log("sum of all natural numbers " + sum);
+
+
+////   Number of a table
+
+// var number = 5; 
+// var i = 1;
+// while (i <= 10) {
+//     console.log(number + " x " + i + " = " + (number * i));
+//     i++;
+// }
