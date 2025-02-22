40ebd5b00b970ff749a7415715924e1be042a2b7
Waqas Ali <waqasansari4547@gmail.com>
2025-02-23 02:47:00 +0500

    Full TODO APP

--- NextJs/counte-app/src/components/Button.js
+++ NextJs/counte-app/src/components/Button.js
@@ -1,8 +1,21 @@
-// import { useState } from "react";
-
-export default function Button({ title , onClick }) {
+export default function Button({
+  title = "submit",
+  type = "button",
+  onClick,
+  bgColor = "blue",
+}) {
   return (
-    <button class="bg-blue-300 text-black font-bold px-10 py-3 m-3 rounded"  onClick={onClick} >
+    <button
+      class={` ${
+        bgColor == "blue"
+          ? "bg-blue-500"
+          : bgColor == "red"
+          ? "bg-red-500"
+          : "bg-orange-500"
+      } bg-blue-300  text-black font-bold px-10 py-3 m-2 rounded`}
+      onClick={onClick}
+      type={type}
+    >
       {title}
     </button>
   );
--- NextJs/counte-app/src/components/TodoApp.js
+++ NextJs/counte-app/src/components/TodoApp.js
@@ -1,34 +1,107 @@
 "use client";
 import { useState } from "react";
+import Button from "./Button";
 
 export function TodoApp() {
   const [input, setInput] = useState("");
-  const [TodoList , setTodoList] = useState([]);
+  const [TodoList, setTodoList] = useState([]);
+  const [isEdit, setIsEdit] = useState(false);
+  const [singleTodo, setSingleTodo] = useState({ text: "", index: "" });
+
+  // Add TodoList
+
   const addTodo = () => {
     console.log("addtodo", input);
-    setTodoList([...TodoList , input]);
+    setTodoList([...TodoList, input]);
     setInput("");
-
   };
 
+  // Delete TodoList
+
+  const deleteTodo = (index) => {
+    console.log("deleteTodo", index);
+    console.log("todolist", TodoList);
+
+    const newTodoList = TodoList.filter(
+      (todo, TodoIndex) => TodoIndex != index
+    );
+    setTodoList(newTodoList);
+  };
+
+  // Edit TodoList
+
+  const editTodo = (item, index) => {
+    setSingleTodo({
+      text: item,
+      index: index,
+    });
+    setInput(item);
+    setIsEdit(true);
+  };
+
+  // Update TodoList
+
+  const updateTodo = () => {
+    console.log("Update Todo");
+
+    const newTodoList = TodoList.map((item , index) =>{
+      console.log("index", index);
+      if (index === singleTodo.index) {
+        return input;
+    }
+    return item;
+  });
+  setTodoList(newTodoList);
+    setInput("");
+    setIsEdit(false);
+    setSingleTodo({ text: "", index: "" });
+};
   return (
     <div className="p-6">
       <form>
         <input
           type="text"
-          class="px-4 py-2 border border-red-500 mt-8"
+          className="px-4 py-2 border border-red-500 mt-8"
           placeholder="Enter any thing"
           value={input}
           onChange={(event) => {
             setInput(event.target.value);
           }}
         />
-        <button type="button" class="bg-blue-600 text-white px-8 py-2 " onClick={addTodo}>Add</button>
+        <Button
+          type="button"
+          onClick={() => {
+            if (!isEdit) {
+              addTodo();
+            } else {
+              updateTodo();
+            }
+          }}
+          bgColor={isEdit ? "orange" : "blue"}
+          title={isEdit ? "Update" : "Add"}
+        />
       </form>
       <ol>
-       {TodoList.map((todo, index)=>{
-        return <li key={index}>{todo}</li>
-       })}
+        {TodoList.map((todo, index) => {
+          return (
+            <li key={index}>
+              <div className="flex gap-4 ms-4">
+                <p className="my-auto">{todo}</p>
+                <div className="flex gap-4"></div>
+                <Button
+                  title="Delete"
+                  onClick={() => deleteTodo(index)}
+                  bgColor="red"
+                />
+                <Button
+                  title="Edit"
+                  onClick={() => editTodo(todo, index)}
+                  bgColor="orange"
+                />
+              </div>
+            </li>
+          );
+        })}
       </ol>
     </div>
   );
